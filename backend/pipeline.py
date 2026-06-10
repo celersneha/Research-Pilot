1b548bfedad90fcdb9087125ba066a9814df7582
Sneha Sharma <snehav2109@gmail.com>
2026-06-11 01:00:36 +0530

    Add writer and critic chains for report generation and evaluation in research pipeline

--- backend/pipeline.py
+++ backend/pipeline.py
@@ -43,4 +43,33 @@ def run_research_pipeline(topic:str) -> dict:
     
     print("\n scraped content\n", state["scraped_content"])
     
+    # step - 3 writer chain
+    print("\n"+" ="*50)
+    print("step 3 - Writer is drafting the report ...")
+    print("="*50)
+    
+    research_combined = (
+        f"SEARCH RESULTS: \n {state["search_results"] }\n\n"
+        f"DETAILED SCRAPED CONTENT: \n {state["scraped_content"]} \n\n"
+    )
+    
+    state["report"] = writer_chain.invoke({
+        "topic":topic,
+        "research" : research_combined
+    })
+    
+    print("\n Final Report \n", state["report"])
+    
+    # critic report
+    print("\n"+" ="*50)
+    print("step 4 - Critic is reviewing the report ...")
+    print("="*50)
+    
+    state["feeedback"] = critic_chain.invoke({
+        "report" : state["report"]
+    })
+    
+    print("\n critic report\n", state["report"])
+    
+    return state
     
